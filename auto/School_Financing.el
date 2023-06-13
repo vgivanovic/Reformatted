5d83754316ddd076f00dcc7c10b11bd90dd4952d
Vladimir G. Ivanović <vladimir@acm.org>
2023-06-12 22:29:15 -0700

    Started tracking the Emacs directory 'auto'.

---
+++ auto/School_Financing.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "School_Financing"
+ (lambda ()
+   (LaTeX-add-labels
+    "ch:ca-school-financing"
+    "fig:LASD_All_Funds_Summary"
+    "fig:net-position"
+    "fig:Change_Position"
+    "fig:Cost_Services"
+    "fig:Capital_Assets"
+    "fig:Long-term_Liabilities"
+    "fig:multi-year-proj"))
+ :latex)
+
