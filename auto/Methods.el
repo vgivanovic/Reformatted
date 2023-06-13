d8e06d8bf2651bee7b6fcfd1ea91ff579d700d50
Vladimir G. Ivanović <vladimir@acm.org>
2023-06-12 22:31:05 -0700

    Started tracking the Emacs directory 'auto'.

---
+++ auto/Methods.el
@@ -0,0 +1,30 @@
+(TeX-add-style-hook
+ "Methods"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "-recorder")
+   (LaTeX-add-labels
+    "ch:methods"
+    "sec:process-overview"
+    "sec:ca-overview"
+    "fig:2019–20_K–12_Funding"
+    "sec:budgets"
+    "sec:lcaps"
+    "sec:CAFRs"
+    "sec:charter-school-financing"
+    "sec:charter-financial-docs"
+    "tab:charter-fin-docs"
+    "sec:petitions-renewals"
+    "sec:other-data"
+    "sec:state-federal-filings"
+    "sec:real-estate"
+    "tab:charter-facilities-options"
+    "sec:gaps-anomalies"
+    "sec:triangulation"
+    "serious-problems"
+    "fig:flow_of_funds_overview"
+    "fig:flow_of_funds_cross-collateralization"
+    "sec:flows-of-money"
+    "fig:opresflows"))
+ :latex)
+
