aaaed2ddcd1e51eb5978025ab19e845395f4e61f
Vladimir G. Ivanović <vladimir@acm.org>
2023-06-13 17:46:00 -0700

    Nightly commit.

--- auto/preamble.el
+++ auto/preamble.el
@@ -7,12 +7,12 @@
                      '(("memoir" "letterpaper" "twoside" "openright" "article" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("lineno" "pagewise") ("caption" "labelfont=rm") ("enumitem" "shortlabels") ("footmisc" "ragged") ("forest" "edges") ("geometry" "left=1.5in" "top=1.25in" "bottom=1.25in" "nomarginpar") ("ulem" "normalem") ("biblatex" "style=apa" "sortcites=false" "sorting=nyt" "backend=biber")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
