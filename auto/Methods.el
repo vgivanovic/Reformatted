7ecd4402b90cbe5642554b30651b346a40df3189
Vladimir G. Ivanović <vladimir@acm.org>
2023-06-17 19:08:46 -0700

    Nightly commit. Almost finished re-writing Chapter 3, Methods.

--- auto/Methods.el
+++ auto/Methods.el
@@ -3,17 +3,17 @@
  (lambda ()
    (setq TeX-command-extra-options
          "-recorder")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
     "ch:methods"
     "sec:process-overview"
-    "sec:ca-overview"
+    "sec:financing-ca-overview"
     "fig:2019–20_K–12_Funding"
     "sec:budgets"
     "sec:lcaps"
@@ -22,12 +22,17 @@
     "sec:charter-financial-docs"
     "tab:charter-fin-docs"
     "sec:petitions-renewals"
+    "sec:lcaps-interim-reports"
     "sec:other-data"
     "sec:state-federal-filings"
     "sec:real-estate"
     "tab:charter-facilities-options"
     "sec:co-locating"
     "sec:leasing"
+    "sec:owning"
+    "tab:paying-for-facilities"
+    "sec:tax-credits"
+    "sec:venture-funds"
     "sec:gaps-anomalies"
     "sec:triangulation"
     "serious-problems"
