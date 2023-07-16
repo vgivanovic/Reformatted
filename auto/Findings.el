4b0b00810cfac0f88cefee5fca2b461b7e90fbb6
Vladimir G. Ivanović <vladimir@acm.org>
2023-07-16 16:09:00 -0700

    Finished (barring typos & blatant errors) Methods.tex

--- auto/Findings.el
+++ auto/Findings.el
@@ -3,22 +3,24 @@
  (lambda ()
    (setq TeX-command-extra-options
          "-recorder")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
     "ch:findings"
     "sec:history"
     "sec:rocketship-corp-struct"
     "fig:corporate-structure"
+    "fig:financial-statements-collected"
     "sec:location-and-property-info"
     "tab:locations"
     "sec:petitions-renewals"
     "sec:staff-reports"
-    "sec:real-estate-data"))
+    "sec:real-estate-data"
+    "fig:opresflows"))
  :latex)
 
