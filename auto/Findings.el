0eb44c61caae7072e854e8f02ee8ce1a88a18329
Vladimir G. Ivanović <vladimir@acm.org>
2023-07-21 08:39:59 -0700

    Draft sent to Roxana on 20-July-2023.

--- auto/Findings.el
+++ auto/Findings.el
@@ -3,24 +3,45 @@
  (lambda ()
    (setq TeX-command-extra-options
          "-recorder")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
     "ch:findings"
     "sec:history"
     "sec:rocketship-corp-struct"
     "fig:corporate-structure"
-    "fig:financial-statements-collected"
+    "sec:findings-charter-financing"
+    "sec:findings-charter-financial-docs"
+    "sec:petitions-renewals"
+    "sec:findings-authorizer-staff-reports"
+    "sec:findings-budgets-etc"
+    "sec:findings-lcaps"
+    "sec:findings-board-material"
+    "sec:findings-charter-real-estate"
     "sec:location-and-property-info"
     "tab:locations"
-    "sec:petitions-renewals"
-    "sec:staff-reports"
-    "sec:real-estate-data"
+    "sec:findings-facilities-options"
+    "sec:findings-co-locating"
+    "sec:findings-leasing"
+    "sec:findings-owning"
+    "sec:findings-funding-ownership"
+    "sec:findings-private-funding"
+    "sec:findings-venture-funds"
+    "sec:findings-tax-credits"
+    "sec:findings-bonds"
+    "sec:findings-other-data"
+    "sec:findings-datasets"
+    "sec:findings-state-federal-filings"
+    "sec:findings-curated-social-media"
+    "sec:findings-gaps-anomolies"
+    "sec:findings-triangulation"
     "fig:opresflows"))
  :latex)
 
--- auto/preamble.el
+++ auto/preamble.el
@@ -6,13 +6,15 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("memoir" "letterpaper" "twoside" "openright" "article" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("babel" "USenglish") ("biblatex" "style=apa" "sortcites=false" "sorting=nyt" "backend=biber") ("caption" "labelfont=rm") ("enumitem" "shortlabels") ("footmisc" "ragged") ("forest" "edges") ("geometry" "left=1.5in" "top=1.25in" "bottom=1.25in" "nomarginpar") ("lineno" "pagewise") ("ulem" "normalem")))
+                     '(("babel" "USenglish") ("biblatex" "style=apa" "sortcites=false" "sorting=nyt" "backend=biber") ("caption" "labelfont=rm") ("enumitem" "shortlabels") ("footmisc" "ragged") ("forest" "edges") ("geometry" "left=1.5in" "right=1in" "top=1.25in" "bottom=1.25in" "nomarginpar") ("lineno" "pagewise") ("rotating" "figureleft") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -42,6 +44,7 @@
     "orcidlink"
     "pgfplots"
     "prettyref"
+    "rotating"
     "ulem"
     "tikz"
     "url"
