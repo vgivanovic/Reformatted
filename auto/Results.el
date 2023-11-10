a75c309138892299505b5ab6d343a2e1cc764c97
Vladimir G. Ivanović <vladimir@acm.org>
2023-11-09 23:27:38 -0800

    Working on Chapter 5: Discussion.

--- auto/Results.el
+++ auto/Results.el
@@ -1,17 +1,23 @@
 (TeX-add-style-hook
  "Results"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (LaTeX-add-labels
     "ch:results"
-    "tab:RocketshipLocations"))
+    "sec:research-question"
+    "sec:summary-key-findings"
+    "sec:appr-answ-rese-quest"
+    "sec:answ-rese-quest"
+    "sec:publ-policy-chang"
+    "sec:issu-future-rese"
+    "sec:conclusion"))
  :latex)
 
