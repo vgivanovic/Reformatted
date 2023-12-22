6970a76aad7c8d289ff515249712edea553220e8
Vladimir G. Ivanović <vladimir@acm.org>
2023-12-21 17:00:57 -0800

    Added index entries for Introduction.tex.

--- auto/Introduction.el
+++ auto/Introduction.el
@@ -3,13 +3,13 @@
  (lambda ()
    (setq TeX-command-extra-options
          "-recorder")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
@@ -17,6 +17,41 @@
     "sec:research-questions"
     "subsec:PublicPolicyFramework")
    (LaTeX-add-index-entries
-    "politics, definition of"))
+    "politics!definition of"
+    "school choice"
+    "school vouchers"
+    "schools!forms of"
+    "school choice!forms of financing"
+    "social impact bonds"
+    "philanthrocapitalism"
+    "privatization!in education"
+    "astroturf"
+    "charter schools!number of U.S."
+    "charter schools!enrollment of in Santa Clara County"
+    "virtual charter schools!expansion of"
+    "charter schools!compared to public schools"
+    "charter schools!out performed by public schools"
+    "charter school chain"
+    "educational management organization"
+    "EMO|see {educational management organization}"
+    "charter management organization"
+    "CMO|see {charter management organization}"
+    "dissertation!goal of"
+    "charter schools!finances of"
+    "Rocketship Public Schools (fn)"
+    "Launchpad LLCs (fn)"
+    "model!Rocketship!adopted by Caliber Public Schools (fn)"
+    "model!Rocketship!adopted by Navigator Schools (fn)"
+    "model!Rocketship|("
+    "model!Rocketship|)"
+    "research question"
+    "charter schools!finances!study of|("
+    "charter schools!finances!study of|)"
+    "dissertation!framework of|("
+    "framework!theoretical|("
+    "framework!theoretical|)"
+    "framework!conceptual|("
+    "framework!conceptual|)"
+    "dissertation!framework of|)"))
  :latex)
 
