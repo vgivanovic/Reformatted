6e5ea332a1d169cc42878dc6f4890e005d5a86c9
Vladimir G. Ivanović <vladimir@acm.org>
2024-01-04 15:14:32 -0800

    Finished indexing of Discussion.tex; draft 0.9.8856 to Roxana.

---
+++ .latexmkrc
@@ -0,0 +1,5 @@
+# .latexmkrc starts
+$pdf_mode = 1;
+$pdflatex = "lualatex --shell-escape %O %S";
+$recorder = 1;
+# .latexmkrc ends
--- auto/Discussion.el
+++ auto/Discussion.el
@@ -3,13 +3,13 @@
  (lambda ()
    (setq TeX-command-extra-options
          "-recorder")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
@@ -27,6 +27,8 @@
     "fig:fraud-tree"
     "sec:real-estate-conv"
     "sec:areas-future-rese"
-    "sec:conclusion"))
+    "sec:conclusion")
+   (LaTeX-add-index-entries
+    "research question"))
  :latex)
 
