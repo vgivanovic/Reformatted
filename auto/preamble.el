5345ec3defbe5bc5d27f38d455d45d67fbb52a3f
Vladimir G. Ivanović <vladimir@acm.org>
2024-01-30 21:48:20 -0800

    Corrected minor errors in Chapter 1.

--- auto/preamble.el
+++ auto/preamble.el
@@ -6,7 +6,7 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("memoir" "letterpaper" "oneside" "article" "12pt" "oldfontcommands")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("babel" "USenglish") ("biblatex" "style=apa" "sortcites=false" "sorting=nyt" "backend=biber") ("caption" "labelfont=rm") ("csquotes" "thresholdtype=words" "threshold=40") ("enumitem" "shortlabels") ("footmisc" "ragged") ("forest" "edges") ("geometry" "left=1.5in" "right=1.0in" "top=1.25in" "bottom=1.25in") ("lineno" "pagewise") ("rotating" "figureleft") ("ulem" "normalem")))
+                     '(("babel" "USenglish") ("biblatex" "style=apa" "sortcites=false" "sorting=nyt" "backend=biber") ("caption" "labelfont=rm") ("csquotes" "thresholdtype=words" "threshold=40") ("enumitem" "shortlabels") ("footmisc" "ragged") ("forest" "edges") ("lineno" "pagewise") ("rotating" "figureleft") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
